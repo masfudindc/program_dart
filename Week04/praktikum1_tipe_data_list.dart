a1eccc8095e1e660cb192b8301ba468a96a4c0af
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-09-27 22:35:45 +0700

    praktikum1 minggu ke-4

---
+++ Week04/praktikum1_tipe_data_list.dart
@@ -0,0 +1,14 @@
+void main(){
+var list = [1, 2, 3];
+assert(list.length == 3);   // Fungsi assert digunakan untuk memastikan bahwa kondisi yang diberikan benar
+assert(list[1] == 2);
+print(list.length);
+print(list[1]);
+
+list[1] = 1;
+assert(list[1] == 1);
+print(list[1]);
+}
+
+
+
