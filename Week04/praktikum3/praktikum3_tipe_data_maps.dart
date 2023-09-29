b77bb334275ef83576bad55cc75999f4867591ce
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-09-29 23:57:45 +0700

    praktikum3 minggu ke-4

---
+++ Week04/praktikum3/praktikum3_tipe_data_maps.dart
@@ -0,0 +1,23 @@
+// Langkah 1
+
+void main(){
+  var gifts = {
+  // Key:    Value
+  'first': 'partridge',
+  'second': 'turtledoves',
+  'fifth': 1
+};
+
+var nobleGases = {
+  2: 'helium',
+  10: 'neon',
+  18: 2,
+};
+
+print(gifts);
+print(nobleGases);
+}
+
+// Langkah 2
+// Program berjalan namun menampilkan kurung kurawal
+
